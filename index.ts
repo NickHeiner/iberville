2ef45b425d239e2f63b29fa8928e0a931152cd36
Nick Heiner <nick.heiner@opower.com>
2015-08-10 17:56:16 -0400

    some initial structure is in place

--- index.ts
+++ index.ts
@@ -1,3 +1,67 @@
-/**
- * Created by nick.heiner on 8/10/15.
- */
+/// <reference path='./typings/tsd.d.ts' />
+
+const qFs = require('q-io/fs'),
+    logger = require('./util/logger'),
+    geoJsonHint = require('geojsonhint');
+
+interface ICreateCityOpts {
+    outFileName: string;
+}
+
+interface IGeoJsonFormatError extends Error {
+    errors: [{
+        message: string;
+        line: number;
+    }];
+}
+
+function createCity(opts: ICreateCityOpts): Q.IPromise<void> {
+    const geoJson = {
+            'type': 'FeatureCollection',
+                'features': [
+                    { 'type': 'Feature',
+                        'geometry': {'type': 'Point', 'coordinates': [102.0, 0.5]},
+                        'properties': {'prop0': 'value0'}
+                    },
+                    { 'type': 'Feature',
+                        'geometry': {
+                            'type': 'LineString',
+                            'coordinates': [
+                                [102.0, 0.0], [103.0, 1.0], [104.0, 0.0], [105.0, 1.0]
+                            ]
+                        },
+                        'properties': {
+                            'prop0': 'value0',
+                            'prop1': 0.0
+                        }
+                    },
+                    { 'type': 'Feature',
+                        'geometry': {
+                            'type': 'Polygon',
+                            'coordinates': [
+                                [ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0],
+                                    [100.0, 1.0], [100.0, 0.0] ]
+                            ]
+                        },
+                        'properties': {
+                            'prop0': 'value0',
+                            'prop1': {'this': 'that'}
+                        }
+                    }
+                ]
+            },
+        errors = geoJsonHint.hint(geoJson);
+
+    if (errors.length) {
+        const err = <IGeoJsonFormatError> new Error('Bug: invalid GeoJson was produced');
+        err.errors = errors;
+        throw err;
+    }
+
+    logger.info({geoJson: geoJson}, 'Writing geojson');
+
+    return qFs.write(opts.outFileName, JSON.stringify(geoJson, null, 2));
+}
+
+module.exports = createCity;
+export = createCity;
