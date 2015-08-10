521cbc1fdff7d67b11b958481fde555fd69d9558
Nick Heiner <nick.heiner@opower.com>
2015-08-10 18:05:06 -0400

    adding get-street-grid

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 /// <reference path='./typings/tsd.d.ts' />
 
+import getStreetGrid = require('./lib/get-street-grid');
+
 const qFs = require('q-io/fs'),
     q = require('q'),
     logger = require('./util/logger'),
@@ -18,39 +20,9 @@ interface IGeoJsonFormatError extends Error {
 
 function createCity(opts: ICreateCityOpts): Q.IPromise<void> {
     const geoJson = {
-            'type': 'FeatureCollection',
-                'features': [
-                    { 'type': 'Feature',
-                        'geometry': {'type': 'Point', 'coordinates': [102.0, 0.5]},
-                        'properties': {'prop0': 'value0'}
-                    },
-                    { 'type': 'Feature',
-                        'geometry': {
-                            'type': 'LineString',
-                            'coordinates': [
-                                [102.0, 0.0], [103.0, 1.0], [104.0, 0.0], [105.0, 1.0]
-                            ]
-                        },
-                        'properties': {
-                            'prop0': 'value0',
-                            'prop1': 0.0
-                        }
-                    },
-                    { 'type': 'Feature',
-                        'geometry': {
-                            'type': 'Polygon',
-                            'coordinates': [
-                                [ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0],
-                                    [100.0, 1.0], [100.0, 0.0] ]
-                            ]
-                        },
-                        'properties': {
-                            'prop0': 'value0',
-                            'prop1': {'this': 'that'}
-                        }
-                    }
-                ]
-            },
+        type: 'FeatureCollection',
+            features: getStreetGrid()
+        },
         errors = geoJsonHint.hint(geoJson);
 
     if (errors.length) {
---
+++ lib/get-street-grid.ts
@@ -0,0 +1,17 @@
+function getStreetGrid() {
+    return [{
+        'type': 'Feature',
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [
+                102,
+                0.5
+            ]
+        },
+        'properties': {
+            'prop0': 'value0'
+        }
+    }];
+}
+
+export = getStreetGrid;
