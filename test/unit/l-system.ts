fd3224bf058d1c8d5733e34b0a0c1db706aea0e8
Nick Heiner <nick.heiner@opower.com>
2015-08-19 17:05:34 -0400

    making test more interesting

--- test/unit/l-system.ts
+++ test/unit/l-system.ts
@@ -9,11 +9,19 @@ interface IT {
     plan: (count: number) => void;
     test: (name: string, testFn: (t: IT) => void) => void;
     equal: (a: any, b: any, message: string) => void;
+    deepEqual: (a: any, b: any, message: string) => void;
 }
 
 tape('lSystem', function(t: IT) {
     t.test('fractal plant', function(t: IT) {
         t.plan(1);
-        t.equal(typeof lSystem, 'function', 'tests work');	
+        
+        const plant = lSystem('X', {
+                X: 'F−[[X]+X]+F[+FX]−X)',
+                F: 'FF'
+            }),
+            time0 = plant.current;
+        
+        t.deepEqual(time0, ['X'], 'starts with axiom');	
     });
 });
