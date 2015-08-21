581c34ed7c98b9a797104c12c73e5247b3502709
Nick Heiner <nick.heiner@opower.com>
2015-08-21 09:46:23 -0400

    scaling blocks relative to their size

--- lib/perturb-street-grid.ts
+++ lib/perturb-street-grid.ts
@@ -3,6 +3,7 @@ import logStep = require('../util/logger/log-step');
 
 const traverse = require('traverse'),
     Alea = require('alea'),
+    turfArea = require('turf-area'),
     _ = require('lodash');
 
 function perturbStreetGrid(
@@ -12,11 +13,22 @@ function perturbStreetGrid(
 
     return logStep({step: 'perturbing street grid', featureLen: featureCollection.features.length}, () => {
         const pRNG = new Alea(opts.seed),
+            // Start with a base amount to perturb by
             perturbAmount = {
                 lat: .0005,
                 long: 0
             },
-            shouldPerturbThreshold = 0;
+
+            // We want the perturbation amount to be relative to the overall size.
+            // This makes it look more realistic. If we try to do a one-size-fits-all
+            // approach, then the largest polys may be only trivially perturbed
+            // relative to their size, and the smallest polys may be totally distorted.
+            perturbAreaCoefficient = .00012,
+
+            // Increase this value to make street be perturbed less often.
+            // 0 = always perturb; 1 = never perturb.
+            // Range: [0, 1]
+            shouldPerturbThreshold = .5;
 
         function getScalingFactor() {
             return pRNG() * 2 - 1;
@@ -24,13 +36,14 @@ function perturbStreetGrid(
 
         return traverse(featureCollection).map(function(node: any) {
             if (this.key === 'coordinates' && this.parent.node.type === 'Polygon' && pRNG() > shouldPerturbThreshold) {
-                logger.warn({
+                logger.debug({
                     node: node
                 }, 'Perturbing node');
 
-                const scaledPerturbAmounts = _.mapValues(
+                const polyArea = turfArea(this.parent.node),
+                    scaledPerturbAmounts = _.mapValues(
                         perturbAmount,
-                        (amount: number) => amount * getScalingFactor()
+                        (amount: number) => amount * getScalingFactor() * polyArea * perturbAreaCoefficient
                     ),
                     coordsClone = <number[][][]> _.cloneDeep(node);
 
